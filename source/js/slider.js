91f50ecb2277be3589e34ebe41903213408bb485
Евраз <kostousik@mail.ru>
2019-12-10 00:18:46 +0300

    last edits

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,72 +1,68 @@
 'use strict';
 
 (() => {
-  const TIMEOUT = 2000;
-  const Key = {
-    ENTER: 'Enter'
-  };
+  const TIMEOUT = 1000;
+  const INTERVAL = 200;
+  const sliderFirst = document.querySelectorAll('.slider__image--first');
+  const sliderSecond = document.querySelectorAll('.slider__image--second');
+  const sliderWebpFirst = document.querySelectorAll('.slider__image-webp-first');
+  const sliderWebpSecond = document.querySelectorAll('.slider__image-webp-second');
+  const sliderToggle = document.querySelectorAll('.slider__button');
 
-  const elFirstImage = document.querySelector('.slider__image--first');
-  const elFirstWebpImage = document.querySelector('.slider__webp-first');
-  const elSecondImage = document.querySelector('.slider__image--second');
-  const elSecondWebpImage = document.querySelector('slider-webp-second');
-  const elsControlButtons = document.querySelectorAll('.slider__toggle-buttons');
-
-  const INTERVAL = 500;
-
-  window.debounce = (cb) => {
+  window.debounce = (cb, ...args) => {
     let lastTimeout = null;
 
     return () => {
-      const parameters = arguments;
+      const parameters = args.sort(function (a, b) { return a - b; });
       if (lastTimeout) {
         window.clearTimeout(lastTimeout);
       }
       lastTimeout = window.setTimeout(() => {
         cb.apply(null, parameters);
-      }, INTERVAL);
+      }, 0);
     };
   };
 
-  const replaceImageAddresses = () => {
-    [elFirstWebpImage.srcset, elSecondWebpImage.srcset] = [elSecondWebpImage.srcset, elFirstWebpImage.srcset];
-    [elFirstImage.srcset, elSecondImage.srcset] = [elSecondImage.srcset, elFirstImage.srcset];
-    [elFirstImage.src, elSecondImage.src] = [elSecondImage.src, elFirstImage.src];
+  const changeImagesplaces = () => {
+    [sliderWebpFirst[0].srcset, sliderWebpSecond[0].srcset] = [sliderWebpSecond[0].srcset, sliderWebpFirst[0].srcset];
+    [sliderFirst[0].srcset, sliderSecond[0].srcset] = [sliderSecond[0].srcset, sliderFirst[0].srcset];
+    [sliderFirst[0].src, sliderSecond[0].src] = [sliderSecond[0].src, sliderFirst[0].src];
   };
 
-  const preventHorizontalScroll = () => {
+  const resetScroll = () => {
     document.body.classList.toggle('js-scroll');
   };
 
-  const changeClassesForAnimation = (isSlow) => {
+  const changeAnimationSpeed = (isSlow) => {
     let postfix = isSlow ? "-slow" : "-fast";
 
-    elFirstImage.classList.remove(`slider__image--first-animation${postfix}`);
-    elFirstImage.classList.add(`slider__image--first-animation${postfix}`);
+    sliderFirst[0].classList.remove(`slider__image--first-animation${postfix}`);
+    sliderFirst[0].classList.add(`slider__image--first-animation${postfix}`);
 
-    elSecondImage.classList.remove(`slider__image--second-animation${postfix}`);
-    elSecondImage.classList.add(`slider__image--second-animation${postfix}`);
-    preventHorizontalScroll();
+    sliderSecond[0].classList.remove(`slider__image--second-animation${postfix}`);
+    sliderSecond[0].classList.add(`slider__image--second-animation${postfix}`);
+    resetScroll();
 
     setTimeout(() => {
-      elFirstImage.classList.remove(`slider__image--first-animation${postfix}`);
-      elSecondImage.classList.remove(`slider__image--second-animation${postfix}`);
-      preventHorizontalScroll();
+      sliderFirst[0].classList.remove(`slider__image--first-animation${postfix}`);
+      sliderSecond[0].classList.remove(`slider__image--second-animation${postfix}`);
+      resetScroll();
     }, TIMEOUT);
   };
 
   const changeImagesFastHandler = debounce(() => {
-    changeClassesForAnimation(false);
-    replaceImageAddresses();
+    changeAnimationSpeed(false);
+    setTimeout(() => {
+    changeImagesplaces();
+    }, INTERVAL);
   });
 
-  const changeImagesFastKeyHandler = debounce((evt) => {
-    if (evt.key === Key.ENTER) {
-      changeClassesForAnimation(false);
-      replaceImageAddresses();
-    }
+  const changeImagesSlowHandler = debounce(() => {
+    changeAnimationSpeed(true);
+    changeImagesplaces();
   });
 
-  elsControlButtons[0].addEventListener('click', changeImagesFastHandler);
-  elsControlButtons[0].addEventListener('keydown', changeImagesFastKeyHandler);
+
+  sliderToggle[0].addEventListener('click', changeImagesFastHandler);
+  sliderToggle[1].addEventListener('click', changeImagesSlowHandler);
 })();
